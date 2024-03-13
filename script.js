7c3eda3bb202a9a16ddb3770adc7a18b54ac2d20
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-03-13 14:47:22 +0530

    product

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ function togglemenu(){
     }
     
 
-}
+};
+
+// swiper
+var swiper = new Swiper(".swiper",{
+    loop:true,
+    centeredSlides:'true',
+    slidesPerView:3,
+    spaceBetween:20,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
